f2f9691a07b607d9802c8025f1a6838e5637a987
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-05-21 23:30:03 +0300

    feat: add expense modal(popup) closes #52

--- types.d.ts
+++ types.d.ts
@@ -10,10 +10,3 @@ declare namespace JSX {
     category: string;
     date: string;
   };
-  type Transaction = {
-    id: number;
-    description: string;
-    date: string;
-    type: string;
-    amount: number;
-};
