fab4e308964db50d63968a77ca126ae875d6b019
Paula-Wang <nimopaula8@gmail.com>
2024-05-26 21:38:49 +0300

    feat: add manage account page

---
+++ app/manageaccount/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import ManageAccount from './ManageAccount';
+
+const ManageAccountPage: NextPage = () => {
+  return (<ManageAccount />);
+}
+
+export default ManageAccountPage;
