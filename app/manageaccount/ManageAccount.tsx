316d506d4baafe071ec3bb514535c321bde8364e
Paula-Wang <nimopaula8@gmail.com>
2024-05-29 09:44:04 +0300

    feat: add delete next to category

--- app/manageaccount/ManageAccount.tsx
+++ app/manageaccount/ManageAccount.tsx
@@ -29,20 +29,30 @@ const ManageAccount: React.FC = () => {
                 <h2 className="text-lg font-semibold mb-0">Income Categories</h2>
                 <button className="px-4 py-2 bg-green-500 text-white rounded-md">Create New Category</button>
               </div>
-              <div className="grid grid-cols-3 gap-6">
-                <div className="bg-gray-200 p-4 rounded-md flex justify-center items-center">
-                  <span>Category 1</span>
-                </div>
-                <div className="bg-gray-200 p-4 rounded-md flex justify-center items-center">
-                  <span>Category 2</span>
-                </div>
-                <div className="bg-gray-200 p-4 rounded-md flex justify-center items-center">
-                  <span>Category 3</span>
-                </div>
+              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
+                <span>Category 2</span>
+                <button className="text-red-500">Delete</button>
               </div>
-              <div className="grid grid-cols-3 gap-6 mt-2">
+              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
+                <span>Category 3</span>
                 <button className="text-red-500">Delete</button>
+              </div>
+            </div>
+            <button className="px-4 py-2 bg-green-500 text-white rounded-md">Create New Category</button>
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold mb-4">Expenses Categories</h2>
+            <div className="grid grid-cols-3 gap-6 mb-4">
+              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
+                <span>Category 1</span>
                 <button className="text-red-500">Delete</button>
+              </div>
+              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
+                <span>Category 2</span>
+                <button className="text-red-500">Delete</button>
+              </div>
+              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
+                <span>Category 3</span>
                 <button className="text-red-500">Delete</button>
               </div>
             </div>
