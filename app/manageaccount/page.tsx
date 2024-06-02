c3bea6543404ca905ea2a9ac1e91395220850080
Aicha Mbongo <zindamoyen2@gmail.com>
2024-06-02 19:08:31 +0300

    feature: add prettier configuration to project

--- app/manageaccount/page.tsx
+++ app/manageaccount/page.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import ManageAccount from './ManageAccount';
 
 const ManageAccountPage: NextPage = () => {
+  return <ManageAccount />;
+};
   return (<ManageAccount />);
 }
 
