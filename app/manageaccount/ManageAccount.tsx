f6532aab0d3b1985cc6c40ccad66430680ccbda1
Paula-Wang <nimopaula8@gmail.com>
2024-06-05 10:56:33 +0300

    fix: remove duplicate expenses categories

--- app/manageaccount/ManageAccount.tsx
+++ app/manageaccount/ManageAccount.tsx
@@ -41,22 +41,6 @@ const ManageAccount: React.FC = () => {
                   Create New Category
                 </button>
               </div>
-              <div className="bg-gray-200 p-4 rounded-md flex justify-between items-center">
-                <span>Category 2</span>
-                <button className="text-red-500">Delete</button>
-                <button className="text-red-500">Delete</button>
-              </div>
-            </div>
-
-            <div className="mb-12 bg-white shadow-md rounded-lg p-6">
-              <div className="flex justify-between items-center mb-4">
-                <h2 className="text-lg font-semibold mb-0">
-                  Expenses Categories
-                </h2>
-                <button className="px-4 py-2 bg-green-500 text-white rounded-md">
-                  Create New Category
-                </button>
-              </div>
               <div className="grid grid-cols-3 gap-6">
                 <div className="bg-gray-200 p-4 rounded-md flex justify-center items-center">
                   <span>Category 1</span>
