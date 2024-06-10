ec3db271d93a68a93b298d9d9898042604481c1c
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-06-10 16:42:32 +0300

    feat: Implemented local database to reduce working on remote

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,7 +2,14 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required.");
+}
+
+//console.log("Supabase URL:", supabaseUrl);
+//console.log("Supabase Key:", supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
---
+++ supabase/seed.sql
@@ -0,0 +1,8 @@
+
+-- in supabase/seed.sql
+insert into
+public.employees (name)
+values
+('Erlich Bachman'),
+('Richard Hendricks'),
+('Monica Hall');
